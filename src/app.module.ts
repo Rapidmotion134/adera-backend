44f018771da97f5f756836e4810297e747027609
Ameen Oumer <ameen.oumer@gmail.com>
2026-01-20 17:16:19 +0300

    milestones added

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,16 +20,16 @@ import { MilestoneModule } from './milestone/milestone.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // host: process.env.DB_HOST,
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DB_HOST,
+      // host: 'localhost',
+      // port: 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      // ssl:
-      //   process.env.NODE_ENV === 'production'
-      //     ? { ca: readFileSync(process.env.DB_CERT_CA).toString() }
-      //     : { rejectUnauthorized: false },
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { ca: readFileSync(process.env.DB_CERT_CA).toString() }
+          : { rejectUnauthorized: false },
       autoLoadEntities: true,
       synchronize: true,
     }),
